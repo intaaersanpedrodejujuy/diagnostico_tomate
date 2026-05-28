9c81f42d95790a9dc1e0224919434f64daf7c12c
intaaersanpedrodejujuy <intaaersanpedrodejujuy@gmail.com>
2026-05-28 13:40:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -85,11 +85,9 @@ async function init() {
     }
 }
 
-// ======================================
-// MENU PARTE PLANTA
-// ======================================
-
 function poblarMenuInicial() {
+
+    alert("ENTRO A poblarMenuInicial");
 
     const params =
         new URLSearchParams(
@@ -99,65 +97,10 @@ function poblarMenuInicial() {
     const locId =
         params.get('loc');
 
-    if (!locId) {
+    alert("LOC = " + locId);
 
-        console.error(
-            'No se recibió localización'
-        );
-
-        return;
-    }
-
-    const select =
-        document.getElementById(
-            'select-localizacion'
-        );
-
-    select.innerHTML = '';
-
-    db.partes.forEach(row => {
-
-        const opt =
-            document.createElement('option');
-
-        opt.value =
-            String(row[0]).trim();
-
-        opt.textContent =
-            row[1];
-
-        select.appendChild(opt);
-    });
-
-    // Selecciona automáticamente la parte elegida
-    select.value = locId;
-
-    // Muestra el nombre en pantalla
-    const parte =
-        db.partes.find(row =>
-            String(row[0]).trim() === locId
-        );
-
-    if (parte) {
-
-        const etiqueta =
-            document.getElementById(
-                'parte-seleccionada'
-            );
-
-        if (etiqueta) {
-
-            etiqueta.textContent =
-                parte[1];
-        }
-    }
-
-    // Fuerza la carga de síntomas
-    select.dispatchEvent(
-        new Event('change')
-    );
+    ...
 }
-
 // ======================================
 // LOCALIZACION -> SINTOMAS
 // ======================================
